e306db478a9b99adff1aeedca069a70ded70d783
palindaa <palinda@xeptagon.com>
2025-07-10 14:49:54 +0100

    sync latest db

--- backup.sh
+++ backup.sh
@@ -3,4 +3,8 @@ AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}
 AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
 BUCKET_NAME=${AWS_BACKUP_BUCKET}
 
-aws s3 cp /app/team_planner.db s3://${BUCKET_NAME}/backups/team_planner_$(date +\%Y\%m\%d\%H\%M\%S).db 
+# Get the latest backup file
+latest_backup=$(aws s3 ls s3://${BUCKET_NAME}/backups/ | grep 'team_planner_' | sort -r | head -n 1 | awk '{print $4}')
+
+# Download the latest backup to current directory
+aws s3 cp s3://${BUCKET_NAME}/backups/$latest_backup /app/
